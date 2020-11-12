061056f35b1eae312aa280bfc58f6fc160400021
amit chhetri <metal.maniacggmu@gmail.com>
2020-11-13 02:10:50 +0530

    Create and update profile routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,7 +17,6 @@ router.post('/', [
 ],
 async (req, res) => {
   const error= validationResult(req);
-  console.log(error);
   if(!error.isEmpty()) {
     return res.status(400).json({errors: error.array() });
   }
