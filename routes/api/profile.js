30835e006c53e883e279f404c205fab8879fdb56
amit chhetri <metal.maniacggmu@gmail.com>
2020-11-21 02:09:50 +0530

    Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -135,7 +135,7 @@ router.get('/user/:user_id', async (req, res) =>{
     }
 })
 // @route  DELETE api/profile
-// @desc   DElete profile, user & posts
+// @desc   Delete profile, user & posts
 // @access Private
 router.delete('/',auth, async (req, res) =>{
     try {
@@ -197,4 +197,26 @@ router.put('/experience', [auth, [
     }
 
 });
+
+// @route  DELETE api/profile/experience/exp_id
+// @desc   Delete experience from profile
+// @access Private
+ router.delete('/experience/:exp_id',auth, async (req, res)=>{
+     try {
+         const profile= await Profile.findOne({user: req.user.id});
+
+         //Get remove index
+         const removeIndex=profile.experience.map(item=>item.id).indexOf(req.params.exp_id);
+       
+         profile.experience.splice(removeIndex,1);
+         
+         await profile.save();
+
+         res.json(profile);
+     } catch (err) {
+         console.error(err.message);
+         res.status(500).send('Server Error');
+     }
+ })
+
 module.exports = router;
